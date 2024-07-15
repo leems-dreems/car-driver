b5cd9735f07a4e458c0fd046bb61a824c3d055c5
wolscott <scottwolcott@gmail.com>
2024-07-15 13:18:07 -0500

    Fires and Sparks on gas pumps
    added preliminary fire and spark effect on gas pump scene

--- collidables/gas_pump.gd
+++ collidables/gas_pump.gd
@@ -1,5 +1,11 @@
 extends RigidBody3D
 
+@onready var fires: Node3D = $Fires
+
 func _on_body_entered(body: Node) -> void:
     if body.is_in_group("CanCrash"):
         apply_impulse(body.global_transform.basis * 100)
+
+func _ready() -> void:
+    for fire in fires.get_children():
+        fire.emitting = true
