32f475159e34bf5669cf6b5eb158ef407c74b087
leems-dreems <130364182+leems-dreems@users.noreply.github.com>
2024-09-07 15:53:35 +0100

    WIP upgrade to 4.3 and add Jolt physics

--- Player/active_ragdoll_skeleton.gd
+++ Player/active_ragdoll_skeleton.gd
@@ -12,6 +12,7 @@ extends Skeleton3D
 @export var max_angular_force: float = 1000.0
 
 var physics_bones: Array[PhysicalBone3D]
+var current_delta := 0.0
 
 
 func _ready() -> void:
@@ -22,6 +23,10 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+  current_delta = delta
+
+
+func _on_skeleton_updated() -> void:
   if player.is_ragdolling:
     return
 
@@ -43,10 +48,38 @@ func _physics_process(delta: float) -> void:
 
     var torque = hookes_law(rotation_difference.get_euler(), b.angular_velocity, angular_spring_stiffness, angular_spring_damping)
     torque = torque.limit_length(max_angular_force)
-    b.target_angular_velocity = torque * delta
+    b.target_angular_velocity = torque * current_delta
 
     b.target_transform = target_transform
   return
+
+
+#func _physics_process(delta: float) -> void:
+  #if player.is_ragdolling:
+    #return
+#
+  #for b in physics_bones:
+    #var _target_bone_transform := target_skeleton.get_bone_global_pose(b.get_bone_id())
+    #var target_transform: Transform3D = target_skeleton.global_transform * _target_bone_transform
+#
+    #var current_transform: Transform3D = global_transform * get_bone_global_pose(b.get_bone_id())
+#
+    #var rotation_difference: Basis = (target_transform.basis * current_transform.basis.inverse())
+    #var position_difference: Vector3 = target_transform.origin - current_transform.origin
+#
+    ##if position_difference.length_squared() > 1.0:
+      ##b.global_position = target_transform.origin
+    ##else:
+    #var force: Vector3 = hookes_law(position_difference, b.linear_velocity, linear_spring_stiffness, linear_spring_damping)
+    #force = force.limit_length(max_linear_force)
+    #b.target_linear_velocity = force
+#
+    #var torque = hookes_law(rotation_difference.get_euler(), b.angular_velocity, angular_spring_stiffness, angular_spring_damping)
+    #torque = torque.limit_length(max_angular_force)
+    #b.target_angular_velocity = torque * delta
+#
+    #b.target_transform = target_transform
+  #return
 
 
 func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, damping: float) -> Vector3:
